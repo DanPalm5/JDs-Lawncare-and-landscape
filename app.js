c78a170f61b4aa21897c4fb673890731ab2c9a0b
Daniel Palmieri <dpalmieri@ycp.edu>
2020-06-24 21:05:48 -0400

    animation timing tweak - smoother now

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const bgimg = document.querySelector(".hero")
 
 const t1 = new TimelineMax();
 
-t1.fromTo(bgimg, 1, {height:"0%"}, 
+t1.fromTo(bgimg, 1.5, {height:"0%"}, 
 {height: "100%", ease: Power2.easeInOut}
 ).fromTo(
     bgimg, 
