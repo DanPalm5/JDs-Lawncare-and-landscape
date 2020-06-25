2044f8ab98e1331c4961943c31bdba06f82f552a
Daniel Palmieri <dpalmieri@ycp.edu>
2020-06-24 20:58:46 -0400

    added current project files

---
+++ app.js
@@ -0,0 +1,12 @@
+const bgimg = document.querySelector(".hero")
+
+const t1 = new TimelineMax();
+
+t1.fromTo(bgimg, 1, {height:"0%"}, 
+{height: "100%", ease: Power2.easeInOut}
+).fromTo(
+    bgimg, 
+    1.2,
+     {width: "80%"},
+    {width: "100%", ease: Power2.easeInOut}
+);
